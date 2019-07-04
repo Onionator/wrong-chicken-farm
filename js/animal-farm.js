5ec8e4c4022e1c8faefdfd9e89f69280ac5306ad
Sam Cook <WhenPotatoesFry@gmail.com>
2019-07-03 23:06:21 -0700

    add a goat class and change the animal-farm.js to work with all new classes.

--- js/animal-farm.js
+++ js/animal-farm.js
@@ -59,14 +59,17 @@ class Game {
 class Animal {
   constructor(serialNumber) {
     this.hunger = 0;
+    this.maxHunger = 0;
     this.readyToProduceOffspring = 0;
-    this.alive = 0;
+    this.readyToProduceOffspringAt = 0;
+    this.value = 0;
     this.serialNumber = serialNumber;
+    this.alive = true;
   }
   // call this function to sell the animal
   sellAnimal() {
     game.animals[game.selectedAnimal].alive = false;
-    game.bank += 10;
+    game.bank += game.animals[game.selectedAnimal].value;
   }
   // call this function to feed the animal
   feedAnimal() {
@@ -82,10 +85,15 @@ class Animal {
   produceOffspring() {
     game.animals[game.selectedAnimal].readyToProduceOffspring = 0;
     let typeOfAnimal = (game.animals[game.selectedAnimal].constructor.name).toString();
-    if (typeOfAnimal === 'Chicken') {
+    if (typeOfAnimal === 'Chicken' && game.selectedAnimal === '7') {
+      game.animals.push(new Goat(game.animals.length));
+      typeOfAnimal = 'Goat';
+    } else if (typeOfAnimal === 'Goat') {
+      game.animals.push(new Goat(game.animals.length));
+    } else if (typeOfAnimal === 'Chicken') {
       game.animals.push(new Chicken(game.animals.length));
     } else {
-      console.log(`log 1- animal-farm.js-- produceOffspring()---\nType of animal is ${typeOfAnimal}. This animal has not yet been defined in the produceOffspring prototype on Animal.`);
+      console.log(`log 1- animal-farm.js-- produceOffspring()---\nType of animal is ${typeOfAnimal}. This animal has not yet been defined in the produceOffspring prototype of Animal.`);
     }
     return typeOfAnimal
   }
@@ -95,15 +103,29 @@ class Animal {
 
 // create a chicken that is an Animal
 class Chicken extends Animal {
-  constructor(serialNumber, hunger, counter, alive) {
-    super(hunger, counter, alive, serialNumber);
+  constructor(serialNumber) {
+    super(serialNumber);
     this.hunger = 20;
-    // add in a feature to include how many times the chicken has to get fed to produce offspring and use that number as the goal to compare readyToProduceOffspring. this is not a number that will need to be changed ever.
+    this.maxHunger = 20;
     this.readyToProduceOffspring = 0;
+    this.readyToProduceOffspringAt = 9;
+    this.value = 10;
     this.serialNumber = serialNumber;
     this.alive = true;
   }
-
+}
+// create a Goat!
+class Goat extends Animal {
+  constructor(serialNumber) {
+    super(serialNumber);
+    this.hunger = 30;
+    this.maxHunger = 30;
+    this.readyToProduceOffspring = 0;
+    this.readyToProduceOffspringAt = 15;
+    this.serialNumber = serialNumber;
+    this.value = 30;
+    this.alive = true;
+  }
 }
 
 
@@ -116,34 +138,30 @@ var game = new Game(new Chicken(0));
 game.gameClock();
 
 function frontEnd() {
-  console.log('log 1- frontEnd');
-  $('.Chicken').removeClass('selected');
+  $('.animal').removeClass('selected');
   $(this).addClass('selected');
   game.selectedAnimal = $(this).attr('value');
 
-  // feed the selected chicken -- even though this statement is false it is still letting our UI feed the chicken
   $('#feed').off().click(function () {
     dyingAnimal(game.selectedAnimal);
     if (game.animals[game.selectedAnimal].alive) {
       game.animals[game.selectedAnimal].feedAnimal();
       updateFeedAndBank();
       updateAnimalsDisplayedStats(game.selectedAnimal);
-      if (game.animals[game.selectedAnimal].readyToProduceOffspring === 9) {
-        game.animals[game.selectedAnimal].produceOffspring();
-        $(`.${(game.animals[game.selectedAnimal].constructor.name)}`).unbind();
-        //add a new chicken to html
+      if (game.animals[game.selectedAnimal].readyToProduceOffspring === game.animals[game.selectedAnimal].readyToProduceOffspringAt) {
+        let tempTypeOfAnimal = game.animals[game.selectedAnimal].produceOffspring();
+        $(`.animal`).unbind();
+        //add a new animal to html
         $('.animalPen').append(`
-          <div class="${(game.animals[game.selectedAnimal].constructor.name)}" value="${game.animals.length - 1}">
-          <p>chicken</p>
+          <div class="animal" value="${game.animals.length - 1}">
+          <p>${tempTypeOfAnimal}</p>
           <span id="${game.animals.length - 1}"></span>
           <label for="meter">Egg</label>
-          <meter value="" min="0" max="9"></meter>
+          <meter value="" min="0" max="${game.animals[game.animals.length -1].readyToProduceOffspringAt}"></meter>
           <label for="meter">Death</label>
-          <meter value="" min="0" max="20"></meter>
+          <meter value="" min="0" max="${game.animals[game.animals.length -1].maxHunger}"></meter>
           </div>`);
-          // make the new chicken clickable
-          $(`.${(game.animals[game.selectedAnimal].constructor.name)}`).bind('click', frontEnd);
-          //
+          $(`.animal`).bind('click', frontEnd);
           $(`#${game.selectedAnimal}`).nextAll().eq(1).attr(`value`, 0);
         }
         $(`#${game.selectedAnimal}`).nextAll().eq(1).attr(`value`, game.animals[game.selectedAnimal].readyToProduceOffspring);
@@ -156,12 +174,12 @@ function frontEnd() {
       game.buyFeed();
       updateFeedAndBank()
     });
-    // console.log(`I fed ${game.selectedAnimal}`);
+
     $('#sell').off().click(function () {
       if (game.animals[game.selectedAnimal].alive) {
         game.animals[game.selectedAnimal].sellAnimal();
         $('.selected').addClass('hidden');
-        $('.Chicken').removeClass('selected');
+        $('.animal').removeClass('selected');
         updateFeedAndBank()
       } else {
         console.log(`chicken ${game.selectedAnimal} is dead. No one wants to buy your disgusting dead chicken.`);
@@ -191,5 +209,5 @@ function frontEnd() {
   }
 
   $(document).ready(function () {
-    $('.Chicken').off().click(frontEnd);
+    $('.animal').off().click(frontEnd);
   });
